c05774d15d6e0252a0053058c01127be15f2e717
Patrick Shim <nasuk0610@gmail.com>
2016-12-28 22:45:20 +0900

    add graphiql

--- src/server/app.js
+++ src/server/app.js
@@ -3,17 +3,19 @@ import mongoose from './util/mongoose.util.js';
 import graphqlHTTP from 'express-graphql';
 import schema from './graphQLschema/index';
 import jwtUtil from './util/jwt.util';
+import config from 'config';
 
 const apiServer = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || config.SERVER.PORT;
 
 mongoose.connect();
 
-apiServer.post('/graphql', jwtUtil.apiProtector, graphqlHTTP((request) => {
+apiServer.use('/graphql', jwtUtil.apiProtector, graphqlHTTP((request) => {
   const startTime = Date.now();
   return {
     schema: schema,
+    graphiql: true,
     rootValue: { request },
     extensions() {
       return { runTime: `${Date.now() - startTime}ms` };
