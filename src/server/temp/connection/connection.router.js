87ea79054f99ca9fb1627556b07829684b82d1e7
Yangwoo Lee <lyw0149@gmail.com>
2017-03-12 13:59:22 +0900

    Delete temp folder.

--- src/server/temp/connection/connection.router.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import jwtUtil from '../util/jwt.util';
-import ConnectionController from './connection.controller';
-
-const router = express.Router();
-
-router.get('/connection', jwtUtil.apiProtector, connectionController.getMyConnections);
-
-export default router;
