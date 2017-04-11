19161d5f2affc714d21ed7d19b4606392adbb4e9
Yangwoo Lee <lyw0149@gmail.com>
2017-04-11 14:54:48 +0900

    Update Braintree util.

--- src/server/util/payment/braintree.util.js
+++ src/server/util/payment/braintree.util.js
@@ -1,10 +1,22 @@
 import braintree from 'braintree';
 
 const gateway = braintree.connect({
-  environment: (process.env.NODE_ENV === 'production') ? braintree.Environment.Production : braintree.Environment.Sandbox,
+  environment: (process.env.NODE_ENV === 'production') ?
+    braintree.Environment.Production :
+    braintree.Environment.Sandbox,
   merchantId: process.env.MERCHANT_ID,
   publicKey: process.env.PUBLIC_KEY,
   privateKey: process.env.PRIVATE_KEY
 });
 
-export default gateway;
+const issueToken = () => new Promise((resolve, reject) => {
+  gateway.clientToken.generate({}, (err, response) => {
+    if (err) return reject(err);
+    return resolve(response.clientToken);
+  });
+});
+
+export {
+  gateway,
+  issueToken
+};
